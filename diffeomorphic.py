3be0728ee1caf05b87776144f946d4f0ee676a7b
Elizabeth Wilson <elizabethnwilson24@gmail.com>
2023-10-17 23:29:29 -0400

    Added basic image support

--- diffeomorphic.py
+++ diffeomorphic.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python
+import argparse
+from math import sin, cos  # sine, cosine used in diffeomoprhic scrambling equation
 import numpy as np
-from math import cos # cosine used in diffeomoprhic scrambling equation
+import pathlib
 from PIL import Image
 
-class ArrayImage():
+
+class ArrayImage:
     def __init__(self, input_image):
-        with Image.open(input_image).convert('RGB') as im: # Original script does not use alpha channel
+        with Image.open(input_image).convert(
+            "RGB"
+        ) as im:  # Original script does not use alpha channel
             # Make image twice the size per original script
             upscaled_width = im.width * 2
             upscaled_height = im.height * 2
-            self._original_image = im.resize((upscaled_width, upscaled_height), resample=Image.Resampling.BILINEAR) # Original
+            self._original_image = im.resize(
+                (upscaled_width, upscaled_height), resample=Image.Resampling.BILINEAR
+            )  # Original
             # Need to get RGB channels into an array of arrays or numpy thing
-            
+
     @property
     def original_image(self):
         return self._original_image
+
+
+def make_new_image(input_image: Image.Image):
+    input_image.save(fp=)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
