2bf1b5a18bc272d9c74706be7659a94b98af3cee
Elizabeth Wilson <elizabethnwilson24@gmail.com>
2023-10-30 18:35:49 -0400

    Tested and expanded save/load feature. Verified to work.

--- diffeomorphic.py
+++ diffeomorphic.py
@@ -7,8 +7,8 @@ from PIL import Image
 
 
 class ArrayImage:
-    def __init__(self, input_image: pl.Path, upscale=True):
-        with Image.open(input_image).convert("RGB") as im:
+    def __init__(self, filepath: pl.Path, upscale=True):
+        with Image.open(filepath).convert("RGB") as im:
             # Original script does not use alpha channel, so only use RGB
             self._original = im
             # Increase image size per original script
@@ -23,7 +23,7 @@ class ArrayImage:
         """
         Returns the result of the diffeomorphic scrambling to be saved.
         Done on demand instead of in __init__() to allow for flexibility
-        and possible more efficient memory usage.
+        and potentially more efficient memory usage.
         """
         ...
 
@@ -40,7 +40,7 @@ class ArrayImage:
         if self._upscaled:
             return self._upscaled
         else:
-            raise SyntaxError("This image has not been upscaled")
+            raise AttributeError("This image has not been upscaled")
 
     @property
     def diffeomorphed(self) -> Image.Image:
@@ -48,58 +48,68 @@ class ArrayImage:
 
 
 class ArrayImageDir:
-    def __init__(self, input_file_or_dir: pl.Path, output_dir: pl.Path):
+    def __init__(self, inputs: list, output_dir: pl.Path):
         """
         Until an operation has been run on self._images, the values are entire ArrayImage objects.
         Afterward, they become images that can be saved.
         """
-        self._input_file_or_dir: pl.Path = input_file_or_dir
+        self._inputs: list = inputs
         self._output_dir: pl.Path = output_dir
+        if self._output_dir != None:
+            if not self._output_dir.exists():
+                self._output_dir.mkdir(parents=True)
         self._accepted_file_types: set = {".jpg", ".png", ".webp"}
+        self._images: dict = {}
 
-        # Creates a dict
-        if input_file_or_dir.is_file():
-            # Need different logic for giving a file versus a dir
-            if input_file_or_dir.suffix in self._accepted_file_types:
-                self._images: dict = {
-                    input_file_or_dir: ArrayImage(input_file_or_dir)
-                }  # Path: ArrayImage
-        else:
-            for file in input_file_or_dir.iterdir():
-                if file.suffix in self._accepted_file_types:
-                    self._images = {file: ArrayImage(file)}
+        # Updates self._images dict
+        for input in self._inputs:
+            if input.is_file():
+                # Logic for an input file
+                if input.suffix in self._accepted_file_types:
+                    self._images |= {input: ArrayImage(input)}  # Path: ArrayImage
+                else:
+                    raise TypeError(
+                        f"Unrecognized file type. Supported file types are: {self._accepted_file_types}"
+                    )
+            else:
+                # Logic for an input dir
+                for filepath in input.iterdir():
+                    if filepath.suffix in self._accepted_file_types:
+                        self._images |= {filepath: ArrayImage(filepath)}
+                    else:
+                        raise TypeError(
+                            f"Unrecognized file type. Supported file types are: {self._accepted_file_types}"
+                        )
 
     def diffeomorph(self):
-        for file in self._images:
-            self._images |= {file: file.diffeomorphed}
+        for filepath, file in self._images.items():
+            self._images |= {filepath: file.diffeomorphed}
 
     def upscale(self):
         """
         ArrayImages are upscaled by default due to the specifications in the original research paper.
         This behavior can be modified by setting upscale=False when instantiating an ArrayImage (such as if you use ArrayImage in a library).
         """
-        for file in self._images:
-            self._images |= {file: file.upscaled}
+        for filepath, file in self._images.items():
+            self._images |= {filepath: file.upscaled}
 
     def save(self):
         """
         Save all files in the output directory
         """
-        for im in self._images:
-            if type(im) == ArrayImage:
+        for filepath, file in self._images.items():
+            if type(file) == ArrayImage:
                 raise TypeError(
-                    "No operation has been run on these files; they will not be saved"
+                    "No operation has been run on these files; they will not be saved."
                 )
-            with im.open():
-                output_file = pl.Path(f"{self._output_dir}/diffeomorphed-{im.name}")
-                im.save(fp=output_file)
+            file.save(pl.Path(f"{self._output_dir}/diffeomorphed-{str(filepath.name)}"))
 
     @property
     def images(self) -> dict:
         return self._images
 
 
-def parse_args():
+def get_args():
     """
     Called regardless of whether script is called on its own or as a library
     since certain things need to be specified every time
@@ -111,44 +121,44 @@ def parse_args():
     # Need option to specify only one file, in which case output directory can still be specified but is assumed to be the same as the file.
     # Output directory if not specified should either make a new directory "{args.input_dir}-diffeomorphed" or just use args.input_dir.
     parser.add_argument(
-        "input_file_or_dir",
-        metavar="INPUT_FILE_OR_DIRECTORY",
+        "inputs",
+        metavar="FILES_OR_DIRECTORIES",
         type=pl.Path,
-        help="Give a path to a file or a directory of files you want to diffeomorph.",
+        nargs="+",
+        help="Give a path to one or more files or one or more directories of files you want to diffeomorph.",
     )
     parser.add_argument(
         "output_dir",
         metavar="OUTPUT_DIRECTORY",
         type=pl.Path,
-        help="Specify an output directory for the new file. Defaults to the input directory or the input file's directory.",
-        default=None,  # None allows us to set it once we have opened the input file or directory
+        help="Specify an output directory for the new file. This is required since you can supply multiple files and directories from different locations.",
     )
     return parser.parse_args()
 
 
-def run_diffeomorph(input_file_or_dir, output_dir):
+def run_diffeomorph(inputs: list, output_dir: pl.Path):
     """
     Uses an input directory or file and an output directory to find images to run the diffeomorphic scrambling on.
     """
-    imdir = ArrayImageDir(input_file_or_dir, output_dir)
+    imdir = ArrayImageDir(inputs, output_dir)
     # Run the diffeomorph for every image in directory
     # imdir.diffeomorph()
     imdir.save()
 
 
-def savetest(input_file_or_dir, output_dir):
-    imdir = ArrayImageDir(input_file_or_dir, output_dir)
+def savetest(inputs: list, output_dir: pl.Path):
+    imdir = ArrayImageDir(inputs, output_dir)
     imdir.upscale()
     imdir.save()
 
 
 def main():
-    args = parse_args()
-    # run_diffeomorph(args.input_file_or_dir, args.output_dir)
-    savetest(args.input_file_or_dir, args.output_dir)
+    args = get_args()
+    # run_diffeomorph(args.inputs, args.output_dir)
+    savetest(args.inputs, args.output_dir)
 
 
 if __name__ == "__main__":
     main()
 else:
-    args = parse_args()
+    args = get_args()  # For if args are supplied to script as a library
